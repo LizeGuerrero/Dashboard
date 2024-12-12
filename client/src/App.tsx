b1eb6cd92a6edc5eeff3c2ecd966630f4c061ad0
LizeGuerrero <lizethguerrero1726@gmail.com>
2024-12-12 11:19:27 -0500

    Front Terminado

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,35 @@
-import './App.scss'
-//import Dashboard from './components/dashboard/Dashboard'
-//import Login from './components/login/Login'
-//import Register from './components/register/Register'
+import './App.scss';
+import Dashboard from './components/dashboard/Dashboard';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
 
 // Import React router dom
 import {
   createBrowserRouter,
   RouterProvider
-} from 'react-router-dom'
+} from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Login</div>
+    element: <Login />
   },
   {
     path: '/register',
-    element: <div>Register</div>
+    element: <Register />
   },
   {
     path: '/dashboard',
-    element: <div>Dashboard</div>
+    element: <Dashboard />
   }
-
-])
+]);
 
 function App() {
-
   return (
     <div>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
