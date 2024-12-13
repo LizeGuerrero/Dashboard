49b3799d9a8176dae298d5dd9ee6aa41423120d4
LizeGuerrero <LIZETHGUERRERO1726@GMAIL.COM>
2024-12-13 16:44:02 -0500

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-// Importar librerías y módulos necesarios
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import path from 'path'; // Importar módulo path para manejar rutas
-import userRoutes from './routes/userRoutes';
-
-// Crear una instancia de la aplicación Express
-const app = express();
-
-// Middleware para manejar CORS y parsear el cuerpo de las solicitudes
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.json());
-
-// Configurar la carpeta de archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Usar las rutas de usuarios
-app.use('/api', userRoutes);
-
-// Ruta para servir el archivo index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Iniciar el servidor en el puerto 3000
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
-});
-=======
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -79,4 +47,3 @@ app.listen(PORT, () => {
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 export default app;
->>>>>>> f0a65fe0bf40fa18fbd0b7bb489f277cf5ea01f5
